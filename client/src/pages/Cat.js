c0abf9806d4aeeb62add74c8797961f7af0280b1
Sainty93 <jenandfloyd93@icloud.com>
2023-03-29 14:09:00 -0700

    initial commit, starting cat/dog page

---
+++ client/src/pages/Cat.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'; 
+import { useMutation } from '@apollo/client'; 
+import { Link } from 'react-router-dom'; 
+
+
+
+function App() {
+    return (
+        <div className="app">
+            <header className="Cats"></header>
+        </div>
+    )
+
+    
+}
+
+    export default App
---
+++ client/src/pages/Dog.js
@@ -0,0 +1,4 @@
+import React, {useState} from 'react'; 
+import { useMutation } from '@apollo/client'; 
+import { Link } from 'react-router-dom'; 
+
