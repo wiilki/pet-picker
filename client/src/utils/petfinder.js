322b176b59eab8a681d5302263ebdbbf4fdc81ec
Will <willkim0608@gmail.com>
2023-04-03 14:30:41 -0700

    Edit petfinder.js

--- client/src/utils/petfinder.js
+++ client/src/utils/petfinder.js
@@ -20,34 +20,35 @@ const petfinder = {
     },
 
     search: async function(searchOptions) {
-        const token = await this.getToken();
-        const headers = {
-          Authorization: `Bearer ${token}`
-        };
-        const queryParams = new URLSearchParams();
-        if (searchOptions.type) {
-          queryParams.append('type', searchOptions.type);
-        }
-        if (searchOptions.breed) {
-          queryParams.append('breed', searchOptions.breed);
-        }
-        if (searchOptions.age) {
-          queryParams.append('age', searchOptions.age);
-        }
-        if (searchOptions.gender) {
-          queryParams.append('gender', searchOptions.gender);
-        }
-        if (searchOptions.location) {
-          queryParams.append('location', searchOptions.location);
-        }
-        const url = `https://api.petfinder.com/v2/animals?${queryParams.toString()}`;
-        const response = await fetch(url, { headers });
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} - ${response.statusText}`);
-        }
-        const data = await response.json();
-        return data;
+      const token = await this.getToken();
+      const headers = {
+        Authorization: `Bearer ${token}`
+      };
+      const queryParams = new URLSearchParams();
+      if (searchOptions.type) {
+        queryParams.append('type', searchOptions.type);
       }
+      if (searchOptions.breed) {
+        queryParams.append('breed', searchOptions.breed);
+      }
+      if (searchOptions.age) {
+        queryParams.append('age', searchOptions.age);
+      }
+      if (searchOptions.gender) {
+        queryParams.append('gender', searchOptions.gender);
+      }
+      if (searchOptions.location) {
+        queryParams.append('location', searchOptions.location);
+      }
+      const url = `https://api.petfinder.com/v2/animals?${queryParams.toString()}`;
+      const response = await fetch(url, { headers });
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+      const data = await response.json();
+      return data;
+    }
+  
     };
     
     export default petfinder;
