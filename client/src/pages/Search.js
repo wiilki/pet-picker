0e466c8910e1710d7bf02662aa374eca22bcd438
Will <willkim0608@gmail.com>
2023-04-05 20:27:21 -0700

    Add logic to generate token from API

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -7,9 +7,12 @@ import PetCard from '../components/PetCard';
 import dogsImage from '../images/dogs.jpg';
 import catsImage from '../images/cats.jpg';
 import rabbitsImage from '../images/rabbits.jpg';
-
-
 import Auth from '../utils/auth';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
 
 const SearchPets = () => {
   // create state for holding returned pet data
@@ -26,24 +29,39 @@ const SearchPets = () => {
     return () => savePetIds(savedPetIds);
   });
 
-  // create function to handle retrieving pet data for the selected animal type
   const handleAnimalType = async (animalType) => {
     try {
-      const response = await fetch(
+      // get access token from the API
+      const tokenResponse = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+      });
+  
+      if (!tokenResponse.ok) {
+        throw new Error('Failed to obtain access token');
+      }
+  
+      const { access_token } = await tokenResponse.json();
+  
+      // use the access token to fetch pet data
+      const petResponse = await fetch(
         `https://api.petfinder.com/v2/animals?type=${animalType}`,
         {
           headers: {
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJXVGFtZkNTZnFoT3M2enRPNkJOMlVXM1dJZkRiOWlKNmhPdndmbWl2M09yT0NuOG92ZyIsImp0aSI6IjVlZmQ3OWQ3ZTVkNDJjMTIwMzZjYWEwOWQ2NmU4NGI0NTdkYTllZTU0OWM5YTJkMWRkOWZlZjEwNTQ0ODg5YTA2ODc4NmQyMjJiNmFiY2UxIiwiaWF0IjoxNjgwNzM4OTA5LCJuYmYiOjE2ODA3Mzg5MDksImV4cCI6MTY4MDc0MjUwOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.FnrZ_oPQDP5HM3KInu0ZFTklruSesLiYEOpGzmRKM3TRcZv0zpHe1x_NBQHA2Dcvp1TsHKrNBqf8yWKHxNJQYvGFT1Tq8SEBjI2Hp5naGfXhJLE97Ya0m2JyeLBV-vdEKN8yO9P6zu3p5Koz1Yf4HmqKEJhzsA_rcN_GlfWzYuFrn9RXt7cCL6fB1tnz179ssxkBQ48xrvckB6lzeF1pM7VKPjTHz-En4kao2OwCTL7K1bXAe1EJ_GvjvuHLO5NQLD9INb-hafLzwBeSD-85NsBgltNknhQx_n5X3IX6i-dPQLpKa_tkEtE3gGsFKwuZQkJge6UrAcqRuYHIYgVrVw`,
+            Authorization: `Bearer ${access_token}`,
           },
         }
       );
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+  
+      if (!petResponse.ok) {
+        throw new Error('Failed to fetch pet data');
       }
-
-      const { animals } = await response.json();
-
+  
+      const { animals } = await petResponse.json();
+  
       const petData = animals.map((pet) => ({
         petId: pet.id,
         name: pet.name,
@@ -53,12 +71,13 @@ const SearchPets = () => {
         description: pet.description,
         image: pet.photos[0]?.medium || '',
       }));
-
+  
       setSearchedPets(petData);
     } catch (err) {
       console.error(err);
     }
   };
+  
 
   // create function to handle saving a pet to our database
   const handleSavePet = async (petId) => {
